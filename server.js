3c8035ca65caf5501ec3e1bdad4352b018ddc655
POSUSA <info@posusa.com>
2025-11-16 11:03:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
